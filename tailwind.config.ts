9bca90a6fc2398d82effb570448a73eea73b225f
asherginting <asherginting3@gmail.com>
2024-10-11 14:29:10 +0700

    feat: setup project & initial dependency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        white: '#FFFFFF',
+        blue: '#4A77FF',
+        cyan: '#40E2E8',
+        green: '#00C696',
+        'light-blue': '#EDFCFF',
+        gray: '#EFF1F6',
+        dark: '#343741',
+        'off-white': '#FBFBFB',
+        'medium-gray': '#5E626F',
+        'charcoal-blue': '#344054',
+        'bright-yellow': '#FBFF23',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
       },
     },
   },
